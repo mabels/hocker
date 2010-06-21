e1faabbed583859403a1241953b8094fa9d50424
Niko Zaft <niko.zaft@sinnerschrader.com>
2010-06-21 14:58:21 +0200

    Modified Hocker for Current Version of node.js.

--- off_hocker.js
+++ off_hocker.js
@@ -5,11 +5,40 @@
 var sys = require('sys')
 var posix = require('fs')
 var http = require('http')
-Buffer = require('buffer').Buffer
+var base64 = require('./base64').base64
+var buffer = require('buffer').Buffer
+var _ = require('./underscore')._
 
-var CouchDB = function() {
-   return http.createClient(5984, '172.16.143.8')
+var CouchDB = function(host, port) {
+   if (port == null) {port = 5984}
+   if (host == null) {host = 'localhost'}
+   return http.createClient(port, host)
 }
+
+
+var BufferWriter = function(fd) {
+   this.fd = fd
+}
+
+BufferWriter.prototype.write = function(buffer, fn, self) {
+   this.written = 0
+   this.buffer = buffer
+   this.to_write = buffer.length
+   this.fn = fn
+   self = this
+   posix.write(this.fd, buffer, this.written, buffer.length, null, function(error,written) { self.writer(error, written) })
+}
+
+BufferWriter.prototype.writer = function(error, written, self) {
+   //sys.puts("WRITTEN: " + written + " THIS.WRITTEN: " + this.written + " TO_WRITE: " + this.to_write)
+   this.written += written
+   if (this.written == this.to_write) {
+      this.fn.apply(this, [null])
+      return
+   }
+   posix.write(this.fd, this.buffer, written, this.to_write - written, null, function(error,written) { self.writer(error,written) })
+}
+
 
 var JSONRequest = function() { }
 
@@ -17,71 +46,131 @@ JSONRequest.prototype.request = function(url, fn, obj, self) {
 //sys.puts("URL: "+url+" FN: "+fn)
    this.chunks = []
    self = this
+   sys.puts('URL_: ' + url)
    obj.http.request('GET',url).addListener('response',function(response) {
       if (response.statusCode == 200) {
+         //sys.puts ('RESPONSE CODE ' + response.statusCode)
          response.setEncoding('utf8')
          response.addListener('data', function(chunk) {
             self.chunks.push(chunk)
          })
          response.addListener('end', function() {
-sys.puts("COMPLETE:"+self.chunks.join(''))
+//sys.puts("COMPLETE:"+self.chunks.join(''))
             //fn(JSON.parse(self.chunks.join('')), obj)
             fn.apply(self, [JSON.parse(self.chunks.join('')), obj])
          })
+      } else {
+         sys.puts('SOMETHING IS REALLY WRONG: ' + response.statusCode + ' ' + url)
       }
    }).end()
 }
 
 JSONRequest.prototype.request_body = function(url, fn, obj, self) {
-sys.puts("request_body:"+url)
+//sys.puts("request_body:"+url)
    this.body = []
    self = this
+   sys.puts('URL_: ' + url)
    obj.http.request('GET',url).addListener('response',function(response) {
       if (response.statusCode == 200) {
+         sys.puts ('SUCCESS ' + response.statusCode + ' ' + url)
          response.addListener('data', function(chunk) {
             self.body.push(chunk)
          })
          response.addListener('end', function() {
             fn.apply(self, [self.body.join(''), obj])
          })
+      } else {
+         sys.puts('SOMETHING IS WRONG: ' + response.statusCode + ' ' + url)
+         response.addListener('end', function() {
+            fn.apply(self, [null, obj])
+         })
       }
    }).end()
 }
+
 var header = function(obj) {
    var msg = obj.length.toString()
    for(var i = msg.length; i < 16; ++i) { msg+=' ' }
    return msg
 }
 
+var remove_attachments = function(obj, data, self) {
+   self = this
+   sys.puts('ROW: ' + data.rows[data.row].id)
+   var json = JSON.parse(obj)
+   sys.puts ('Attachments: ' + json._attachments)
+   var keys = _(json._attachments).keys()
+   var call_it = function(k, att_chunk) {
+      att_chunk = []
+      data.http.request('GET','/'+data.db+'/'+data.rows[data.row].id+'/'+k)
+          .addListener('response',function(response) {
+           response.setEncoding('binary')
+           sys.puts('GET SUCCESS ' + response.statusCode + ' / ' + k);
+            response.addListener('data', function(chunk) {
+               att_chunk.push(chunk)
+            })
+            response.addListener('end',function() {
+               json._attachments[k].stub = false
+               sys.puts('Stub: '+json._attachments[k].stub)
+               json._attachments[k].data = base64.encode(att_chunk.join(''))
+
+               //sys.puts ("haha: "+ sys.inspect(json._attachments[k]))
+               //json._attachments.content_type = req.respodnfbgfghkf
+               if (keys.length) {
+                  call_it(keys.shift())
+               } else {
+                  sys.puts('Header: '+sys.inspect(data.rows[data.row].id)+' , '+sys.inspect(JSON.stringify(json).length))
+                  var msg = header(data.rows[data.row].id)+data.rows[data.row].id+header(JSON.stringify(json))
+                  var buffer_head = new buffer(msg)
+                  var buffer_body = new buffer(JSON.stringify(json))
+                  new BufferWriter(data.file).write(buffer_head,function(){
+                     new BufferWriter(data.file).write(buffer_body,function(){
+                        data.row += 1
+                        if (data.row < data.rows.length) {
+                           self.request_body('/'+data.db+'/'+data.rows[data.row].id+'?attachments=true', get_document, data)
+                        } else {
+                           self.request('/'+data.db+'/_all_docs?limit=1000&skip=1&startkey_docid='+(data.rows[data.rows.length-1].id), all_docs_reader, data)
+                        }
+                     })
+                  })
+               }
+            })
+      }).end()
+   }
+   call_it(keys.shift())
+}
 
 var get_document = function(obj, data, self) {
    self = this
-sys.puts("get_document_write_header:start:"+data.file+":"+data.rows[data.row].id)
-   var msg = header(data.rows[data.row].id)+data.rows[data.row].id+header(obj)
-   sys.puts("MSG: " + msg + " OBJ: " + obj + " Obj_type: " + obj)
-    var x = posix.write(data.file, new Buffer(msg), 0, msg.length, null, function(err, written) {
-
-
-sys.puts ("ERR: " + err + " WRITTEN: " + written + " LENGTH: " + msg.length)
-   
-//sys.puts("get_document_write_header:done")
-//sys.puts("get_document_write_data:start")
-      sys.puts("***OBJ-LENGTH: " + obj.length)
-      posix.write(data.file, new Buffer(obj), 0, obj.length, null, function() {
-sys.puts("get_document_write_data:done") //ad(parseInt(length),
-         data.row += 1
-         if (data.row < data.rows.length) {
-            self.request_body('/'+data.db+'/'+data.rows[data.row].id+'?attachments=true', get_document, data)
-         } else {
-            self.request('/'+data.db+'/_all_docs?limit=1000&skip=1&startkey_docid='+(data.rows[data.rows.length-1].id), all_docs_reader, data)
-        }
+   if (obj != null){
+      //sys.puts('ROW: ' + data.rows[data.row].id)
+      var msg = header(data.rows[data.row].id)+data.rows[data.row].id+header(obj)
+      var buffer_head = new buffer(msg)
+      var buffer_body = new buffer(obj)
+      
+   //sys.puts("MSG: " + msg + " OBJ: " + obj)
+      new BufferWriter(data.file).write(buffer_head,function(){
+         new BufferWriter(data.file).write(buffer_body,function(){
+            data.row += 1
+            if (data.row < data.rows.length) {
+               self.request_body('/'+data.db+'/'+data.rows[data.row].id+'?attachments=true', get_document, data)
+            } else {
+               self.request('/'+data.db+'/_all_docs?limit=1000&skip=1&startkey_docid='+(data.rows[data.rows.length-1].id), all_docs_reader, data)
+            }
+         })
       })
-   })
-   sys.puts("WRITE RES: " + x)
+   } else {
+      //try without attachments
+      if (data.row < data.rows.length) {
+         self.request_body('/'+data.db+'/'+data.rows[data.row].id+'?attachments=false', remove_attachments, data)
+      } else {
+         self.request('/'+data.db+'/_all_docs?limit=1000&skip=1&startkey_docid='+(data.rows[data.rows.length-1].id), all_docs_reader, data)
+      }
+   }
 }
 
 var all_docs_reader = function(obj, data) { 
-sys.puts("ALL DATA: "+data)
+//sys.puts("ALL DATA: "+ sys.inspect(data))
 if (obj.rows.length > 0) {
       data.cnt += obj.rows.length
       sys.puts("next:"+data.db+"="+data.cnt+"=>"+(obj.rows[obj.rows.length-1].id))
@@ -90,15 +179,29 @@ if (obj.rows.length > 0) {
       this.request_body('/'+data.db+'/'+data.rows[data.row].id+'?attachments=true', get_document, data)
    } else {
       posix.close(data.file, function() {
-         sys.puts("COMPLETE:"+data.db)
+         //sys.puts("COMPLETE:"+data.db)
       })
    }
 }
 
+//var split_db_name = function(db){
+//   var Suche = /([\w\.]*):(\d+\d+\d+\d+)\/(\w*)/
+//   var Ergebnis = Suche.exec(db)
+//   if (Ergebnis){
+//      host = Ergebnis[1]
+//      port = Ergebnis[2]
+//      db_name = Ergebnis[3]
+//   } else {
+//      host = null
+//      port = null
+//      db_name = db
+//   }
+//   return [host, port, db_name]
+//}
 
-var Hocker = function(db) {
+var Hocker = function(db,host,port) {
    this.db = db
-   this.http = CouchDB()
+   this.http = CouchDB(host,port)
    var self = this
    posix.open(this.db+".hocker", process.O_WRONLY | process.O_TRUNC | process.O_CREAT, 0644, function(err, fd) {
       self.file = fd
@@ -109,8 +212,8 @@ sys.puts("MADE .hocker for: " + self.err +" "+self.file+"="+self.db)
    })
 }
 
-for (var i = process.ARGV.length-1; i >= 2; --i) {
-   new Hocker(process.ARGV[i])
+for (var i = process.ARGV.length-1; i >= 4; --i) {
+   new Hocker(process.ARGV[i],process.ARGV[2],process.ARGV[3])
 }
 /*
 var all_dbs = new JSONRequest() 
--- on_hocker.js
+++ on_hocker.js
@@ -5,86 +5,93 @@
  */
 
 var sys = require('sys')
-var posix = require('posix')
+var posix = require('fs')
 var http = require('http')
+var buffer = require('buffer').Buffer
 
-var CouchDB = function() {
-   return http.createClient(5984, 'localhost') 
+var CouchDB = function(host, port) {
+   if (port == null) {port = 5984}
+   if (host == null) {host = 'localhost'}
+   return http.createClient(port, host)
 }
 
 var BufferReader = function(fd) {
    this.fd = fd
 }
 
-BufferReader.prototype.read = function(toread, fn, self) {
-   this.toread = toread
+BufferReader.prototype.read = function(buffer, fn, self) {
+   this.buffer = buffer
+   this.to_read = buffer.length
    this.done_read = 0
-   this.buffer = []
-   this.toread = toread
    this.fn = fn
    self = this
 //sys.puts('BufferReader.prototype.read:'+this.fd+":"+this.toread)
-   posix.read(this.fd, this.toread).addCallback(function(d,r) { self.reader(d,r) })
+   posix.read(this.fd, this.buffer, this.done_read, this.to_read, null, function(err,bytes_read) { self.reader(err,bytes_read) })
 }
 
-BufferReader.prototype.reader = function(data, read, self) {
-   if (read == 0) { 
+BufferReader.prototype.reader = function(err, bytes_read, self) {
+   if (bytes_read == 0) {
       this.fn.apply(this, [null])
       return
    }
-   this.done_read += read
-   this.buffer.push(data) 
-   if (this.done_read == this.toread) { 
-//sys.puts("BufferReader.prototype.reader:apply="+this.done_read)
-      this.fn.apply(this, [this.buffer.join('')])
+   this.done_read += bytes_read
+   if (this.done_read == this.to_read) {
+      this.fn.apply(this, [this.buffer.toString('utf8', 0, this.to_read)])
    } else {
       self = this
-      posix.read(this.fd, this.toread - this.done_read).addCallback(function(d,r) { self.reader(d,r) })
+      var length = this.to_read - this.done_read
+      posix.read(this.fd, this.buffer, this.done_read, length, null, function(err, bytes_read) { self.reader(err,bytes_read) })
    }
 }
 
-
-var OnHocker = function(fname) {
+var OnHocker = function(fname, host, port) {
    this.fname = fname
    this.db = "hocker_"+fname.replace(/\.hocker$/,'')
    this.cnt = 0
    var self = this
 sys.puts("OnHocker:START:CREATEDB:"+self.db)
-   var req = CouchDB().del('/'+self.db+'/', { 'Content-Length': 0 })
-   req.sendBody('')
-   req.finish(function() { 
-      req = CouchDB().put('/'+self.db+'/', { 'Content-Length': 0 })
-      req.sendBody('')
-      req.finish(function() { 
+   var req = CouchDB(host, port).request('DELETE','/'+self.db+'/', { 'Content-Length': 0 })
+   req.write('')
+   req.addListener('response',function() {
+sys.puts("CREATE DB. " + self.db)
+      req = CouchDB().request('PUT','/'+self.db+'/', { 'Content-Length': 0 })
+      req.write('')
+      req.addListener('response',function() {
 //sys.puts("OnHocker:START:OPEN:"+self.fname)
-         posix.open(self.fname, process.O_RDONLY, 0644).addCallback(function(fd) {
-//sys.puts("OPENED:"+self.db)
+         posix.open(self.fname, process.O_RDONLY, 0644, function(err, fd) {
+//sys.puts("OPENED:"+self.fd)
             self.put_object(fd)
          })
-      })
-   })
+      }).end()
+   }).end()
 }
+
 OnHocker.prototype.put_object = function(fd, self) {
    self = this
-   new BufferReader(fd).read(16, function(length) {
+   new BufferReader(fd).read(new buffer(16), function(length) {
       if (length == null) { 
          sys.puts('COMPLETED:'+self.cnt+":"+self.db)
          return 
       }
-//sys.puts("OnHocker.prototype.put_object:"+this.done_read)
-      this.read(parseInt(length), function(id) {
-//sys.puts('ID:'+id)
-         this.read(16, function(length) {
-            length = parseInt(length)
-            this.read(length, function(body) {
+sys.puts("OnHocker.prototype.put_object:"+this.done_read+" : "+length)
+      this.read(new buffer(parseInt(length,10)), function(id) {
+sys.puts('ID:'+id)
+         this.read(new buffer(16), function(length) {
+            length = parseInt(length,10)
+            this.read(new buffer(length), function(body) {
 //sys.puts('PUT:'+length+":"+body)
-               req = CouchDB().put('/'+self.db+'/'+id, {'Content-Length': length.toString()})
-               req.sendBody(body, 'binary');
-               req.finish(function(response) {
+               body = JSON.parse(body)
+               delete body._rev
+               body = JSON.stringify(body)
+               req = CouchDB().request('PUT','/'+self.db+'/'+id, {'Content-Length': body.length.toString()})
+               sys.puts('Body Length: ' + body.length + 'Buffer Length: ' +length.toString())
+               req.write(body, 'binary');
+               req.addListener('response', function(response) {
+sys.puts ('STATUS ' + response.statusCode)
 //sys.puts('DONE:'+length+":"+body)
                   self.cnt += 1
-                  self.put_object(fd)       
-               })
+                  self.put_object(fd)
+               }).end()
             })
          })
       })
@@ -92,7 +99,7 @@ OnHocker.prototype.put_object = function(fd, self) {
 }
 
 var couchdb = CouchDB()
-for (var i = process.ARGV.length-1; i >= 2; --i) {
-   new OnHocker(process.ARGV[i])
+for (var i = process.ARGV.length-1; i >= 4; --i) {
+   new OnHocker(process.ARGV[i],process.ARGV[2],process.ARGV[3]);
 }
 
